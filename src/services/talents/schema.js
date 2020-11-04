bf8a30d3c133bfa917260e31a994404460a573e3
Solomon <solomon6u@yahoo.com>
2020-11-04 19:50:17 +0100

    Update schema.js

--- src/services/talents/schema.js
+++ src/services/talents/schema.js
@@ -14,6 +14,10 @@ const TalentSchema = new Schema({
         type: String,
         required: true,
     },
+    techStack: {
+        type: String,
+        required: true,
+    },
     email: {
         type: String,
         required: true,
